f06d0e7b1fd642bad37388e76d69fced1308359d
omer_sm <smorodinskyomer@gmail.com>
2025-07-31 05:33:44 +0300

    first commit

---
+++ lib/tamnoon_qr.ex
@@ -0,0 +1,18 @@
+defmodule TamnoonQr do
+  @moduledoc """
+  Documentation for `TamnoonQr`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> TamnoonQr.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/tamnoon_qr_test.exs
@@ -0,0 +1,8 @@
+defmodule TamnoonQrTest do
+  use ExUnit.Case
+  doctest TamnoonQr
+
+  test "greets the world" do
+    assert TamnoonQr.hello() == :world
+  end
+end
